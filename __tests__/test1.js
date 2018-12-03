77fd4aa68e74cde5554b4da6e48047c5831faecf
MathDodo <mathiasdodo@hotmail.com>
2018-12-03 22:10:56 +0100

    - yooooo updates

--- __tests__/test1.js
+++ __tests__/test1.js
@@ -13,54 +13,112 @@ describe(
       await page.close();
     })
 //Decision and condition 
-    let test1 = "test bmi output overweight"
-    it(test1, async () => {
-      await page.waitFor('input[name=cm]');
-      await page.$eval('input[name=cm]', el => el.value = "180");
-      await page.waitFor('input[name=kg]');
-      await page.$eval('input[name=kg]', el => el.value = "92");
-      await page.click('input[type=submit]');
-      
-      await page.waitFor(500);
-      const myResult1 = await page.$("#bmiVal");
-      const txt = await page.evaluate(myResult => myResult.textContent, myResult1);
 
-      await expect(txt).toBe("28.4");
-      //await expect(page.url()).toBe("http://localhost:8080/result?cm=180&kg=92");
-    }, timeout)
+let test1 = "m = 0"
+it(test1, async () => {
+  await page.waitFor('input[name=cm]');
+  await page.$eval('input[name=cm]', el => el.value = "0");
+  await page.waitFor('input[name=kg]');
+  await page.$eval('input[name=kg]', el => el.value = "92");
+  await page.click('input[type=submit]');
+  
+  await page.waitFor(200);    
+  await expect(page.url()).toBe("http://localhost:8080/");
+}, timeout)
+
+//this test, tests a low bmi output
+let test2 = "nan m"
+it(test2, async () => {
+  await page.waitFor('input[name=cm]');
+  await page.$eval('input[name=cm]', el => el.value = "Hello world");
+  await page.waitFor('input[name=kg]');
+  await page.$eval('input[name=kg]', el => el.value = "55");
+  await page.click('input[type=submit]');
+  
+  await page.waitFor(200);
+  await expect(page.url()).toBe("http://localhost:8080/");
+}, timeout)
+
+//this test, tests a low bmi output
+let test3 = "m not zero and a number"
+it(test3, async () => {
+await page.waitFor('input[name=cm]');
+await page.$eval('input[name=cm]', el => el.value = "186");
+await page.waitFor('input[name=kg]');
+await page.$eval('input[name=kg]', el => el.value = "75");
+await page.click('input[type=submit]');
+
+await page.waitFor(200);
+const myResult3 = await page.$("#bmiVal");
+const txt = await page.evaluate(myResult => myResult.textContent, myResult3);
+await expect(txt).toBe("21.68");
+}, timeout)
+
+let test4 = "bmiCat"
+it(test4, async () => {
+await page.waitFor('input[name=cm]');
+await page.$eval('input[name=cm]', el => el.value = "186");
+await page.waitFor('input[name=kg]');
+await page.$eval('input[name=kg]', el => el.value = "75");
+await page.click('input[type=submit]');
+
+await page.waitFor(200);
+const myResult3 = await page.$("#bmiCat");
+const txt = await page.evaluate(myResult => myResult.textContent, myResult3);
+await expect(txt).toBe("Healthy weight");
+}, timeout)
+
+//This region contains our trials of testing
+//#region 
+    // let test4 = "test bmi output overweight"
+    // it(test4, async () => {
+    //   await page.waitFor('input[name=cm]');
+    //   await page.$eval('input[name=cm]', el => el.value = "180");
+    //   await page.waitFor('input[name=kg]');
+    //   await page.$eval('input[name=kg]', el => el.value = "92");
+    //   await page.click('input[type=submit]');
+      
+    //   await page.waitFor(250);
+    //   const myResult1 = await page.$("#bmiVal");
+    //   const txt = await page.evaluate(myResult => myResult.textContent, myResult1);
+
+    //   await expect(txt).toBe("28.4");
+    //   //await expect(page.url()).toBe("http://localhost:8080/result?cm=180&kg=92");
+    // }, timeout)
 
     //this test, tests a low bmi output
-    let test2 = "test bmi output underweight"
-    it(test2, async () => {
-      await page.waitFor('input[name=cm]');
-      await page.$eval('input[name=cm]', el => el.value = "180");
-      await page.waitFor('input[name=kg]');
-      await page.$eval('input[name=kg]', el => el.value = "55");
-      await page.click('input[type=submit]');
+//     let test5 = "test bmi output underweight"
+//     it(test5, async () => {
+//       await page.waitFor('input[name=cm]');
+//       await page.$eval('input[name=cm]', el => el.value = "180");
+//       await page.waitFor('input[name=kg]');
+//       await page.$eval('input[name=kg]', el => el.value = "55");
+//       await page.click('input[type=submit]');
       
-      await page.waitFor(500);
-      const myResult2 = await page.$("#bmiVal");
-      const txt = await page.evaluate(myResult => myResult.textContent, myResult2);
-//console.log(txt);
-      await expect(txt).toBe("16.98");
-    }, timeout)
+//       await page.waitFor(250);
+//       const myResult2 = await page.$("#bmiVal");
+//       const txt = await page.evaluate(myResult => myResult.textContent, myResult2);
+// //console.log(txt);
+//       await expect(txt).toBe("16.98");
+//     }, timeout)
 
- //this test, tests a low bmi output
- let test3 = "test bmi output normal weight"
- it(test3, async () => {
-   await page.waitFor('input[name=cm]');
-   await page.$eval('input[name=cm]', el => el.value = "186");
-   await page.waitFor('input[name=kg]');
-   await page.$eval('input[name=kg]', el => el.value = "75");
-   await page.click('input[type=submit]');
+//  //this test, tests a low bmi output
+//  let test6 = "test bmi output normal weight"
+//  it(test6, async () => {
+//    await page.waitFor('input[name=cm]');
+//    await page.$eval('input[name=cm]', el => el.value = "186");
+//    await page.waitFor('input[name=kg]');
+//    await page.$eval('input[name=kg]', el => el.value = "75");
+//    await page.click('input[type=submit]');
    
-   await page.waitFor(500);
-   const myResult3 = await page.$("#bmiVal");
-   const txt = await page.evaluate(myResult => myResult.textContent, myResult3);
-//console.log(txt);
-   await expect(txt).toBe("21.68");
- }, timeout)
-
+//    await page.waitFor(250);
+//    const myResult3 = await page.$("#bmiVal");
+//    const txt = await page.evaluate(myResult => myResult.textContent, myResult3);
+// //console.log(txt);
+//    await expect(txt).toBe("21.68");
+//  }, timeout)
+//#endregion
   },
   timeout
 )
+
--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get("/result", (req, res) =>
 {
     var m = (parseInt(req.query.cm, 10) / 100);
     var kg = req.query.kg;
+    var wcm = req.query.wcm;
+    var hcm = req.query.hcm;
+    var gender = req.query.Gender;        
+    var whrCategory = "";
 
     if(m == 0 || isNaN(m))
     {
@@ -24,11 +28,60 @@ app.get("/result", (req, res) =>
     }
     else 
     {
-        var bmi = (kg / (m * m));
+        var bmi = (kg / (m * m));        
+        var bmiCat = "Underweight";
+        var whrVal = wcm / hcm;
+
+        if(bmi >= 18.5 && bmi <= 24.9)
+        {
+            bmiCat = "Healthy weight";
+        }
+        else if(bmi >= 25 && bmi <= 29.9)
+        {
+            bmiCat = "Overweight";
+        }
+        else if(bmi >= 30)
+        {
+            bmiCat = "Obese";
+        }
+
+        if(gender == "Male")
+        {
+            if(whrVal < .90)
+            {
+                whrCategory = "Normal weight";
+            }
+            else if(whrVal >= .90 && whrVal < .99)
+            {
+                whrCategory = "Overweight";
+            }
+            else
+            {
+                whrCategory = "Obesity is your goal in life";
+            }
+        }
+        else
+        {
+            if(whrVal < .80)
+            {
+                whrCategory = "Normal weight";
+            }
+            else if(whrVal >= .80 && whrVal < .84)
+            {
+                whrCategory = "Overweight";
+            }
+            else
+            {
+                whrCategory = "Obesity is your goal in life or you might be pregnant";
+            }
+        }
 
         res.render("pages/result", 
         {
-            bmi: bmi
+            bmi: bmi,
+            bmiCategory: bmiCat,
+            whr: whrVal,
+            whrCat: whrCategory
         });
     }
 
